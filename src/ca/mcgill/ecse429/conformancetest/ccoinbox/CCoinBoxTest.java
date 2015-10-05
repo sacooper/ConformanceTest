7a766bc81f8541b20b5e829ea2f7c5a13055aa26
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2015-10-05 14:07:29 -0400

    Added verifying actions occured

--- src/ca/mcgill/ecse429/conformancetest/ccoinbox/CCoinBoxTest.java
+++ src/ca/mcgill/ecse429/conformancetest/ccoinbox/CCoinBoxTest.java
@@ -14,12 +14,22 @@ public class CCoinBoxTest {
   }
   @Test
   public void testCase0() {
+    Object o2_0 = klazz.getCurQtrs() + 1;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o2_0);
     assertEquals("notAllowed", klazz.getStateFullName());
+    Object o5_0 = klazz.getCurQtrs() + 1;
+    Object o5_1 = true;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o5_0);
+    assertEquals(klazz.getAllowVend() , o5_1);
     assertEquals("allowed", klazz.getStateFullName());
     klazz.setCurQtrs(3 + 1);
+    Object o11_0 = klazz.getTotalQtrs() + 2;
+    Object o11_1 = klazz.getCurQtrs() - 2;
     klazz.vend();
+    assertEquals(klazz.getTotalQtrs() , o11_0);
+    assertEquals(klazz.getCurQtrs() , o11_1);
     assertEquals("allowed", klazz.getStateFullName());
   }
 
@@ -31,75 +41,141 @@ public class CCoinBoxTest {
 
   @Test
   public void testCase2() {
+    Object o1_0 = 0;
     klazz.reset();
+    assertEquals(klazz.getTotalQtrs() , o1_0);
     assertEquals("empty", klazz.getStateFullName());
   }
 
   @Test
   public void testCase3() {
+    Object o2_0 = klazz.getCurQtrs() + 1;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o2_0);
     assertEquals("notAllowed", klazz.getStateFullName());
+    Object o3_0 = 0;
     klazz.returnQtrs();
+    assertEquals(klazz.getCurQtrs() , o3_0);
     assertEquals("empty", klazz.getStateFullName());
   }
 
   @Test
   public void testCase4() {
+    Object o2_0 = klazz.getCurQtrs() + 1;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o2_0);
     assertEquals("notAllowed", klazz.getStateFullName());
+    Object o4_0 = 0;
+    Object o4_1 = 0;
     klazz.reset();
+    assertEquals(klazz.getTotalQtrs() , o4_0);
+    assertEquals(klazz.getCurQtrs() , o4_1);
     assertEquals("empty", klazz.getStateFullName());
   }
 
   @Test
   public void testCase5() {
+    Object o2_0 = klazz.getCurQtrs() + 1;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o2_0);
     assertEquals("notAllowed", klazz.getStateFullName());
+    Object o5_0 = klazz.getCurQtrs() + 1;
+    Object o5_1 = true;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o5_0);
+    assertEquals(klazz.getAllowVend() , o5_1);
     assertEquals("allowed", klazz.getStateFullName());
+    Object o6_0 = 0;
+    Object o6_1 = false;
     klazz.returnQtrs();
+    assertEquals(klazz.getCurQtrs() , o6_0);
+    assertEquals(klazz.getAllowVend() , o6_1);
     assertEquals("empty", klazz.getStateFullName());
   }
 
   @Test
   public void testCase6() {
+    Object o2_0 = klazz.getCurQtrs() + 1;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o2_0);
     assertEquals("notAllowed", klazz.getStateFullName());
+    Object o5_0 = klazz.getCurQtrs() + 1;
+    Object o5_1 = true;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o5_0);
+    assertEquals(klazz.getAllowVend() , o5_1);
     assertEquals("allowed", klazz.getStateFullName());
+    Object o7_0 = 0;
+    Object o7_1 = 0;
+    Object o7_2 = false;
     klazz.reset();
+    assertEquals(klazz.getTotalQtrs() , o7_0);
+    assertEquals(klazz.getCurQtrs() , o7_1);
+    assertEquals(klazz.getAllowVend() , o7_2);
     assertEquals("empty", klazz.getStateFullName());
   }
 
   @Test
   public void testCase7() {
+    Object o2_0 = klazz.getCurQtrs() + 1;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o2_0);
     assertEquals("notAllowed", klazz.getStateFullName());
+    Object o5_0 = klazz.getCurQtrs() + 1;
+    Object o5_1 = true;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o5_0);
+    assertEquals(klazz.getAllowVend() , o5_1);
     assertEquals("allowed", klazz.getStateFullName());
+    Object o8_0 = klazz.getCurQtrs() + 1;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o8_0);
     assertEquals("allowed", klazz.getStateFullName());
   }
 
   @Test
   public void testCase8() {
+    Object o2_0 = klazz.getCurQtrs() + 1;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o2_0);
     assertEquals("notAllowed", klazz.getStateFullName());
+    Object o5_0 = klazz.getCurQtrs() + 1;
+    Object o5_1 = true;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o5_0);
+    assertEquals(klazz.getAllowVend() , o5_1);
     assertEquals("allowed", klazz.getStateFullName());
     klazz.setCurQtrs(2);
+    Object o9_0 = klazz.getTotalQtrs() + 2;
+    Object o9_1 = 0;
+    Object o9_2 = false;
     klazz.vend();
+    assertEquals(klazz.getTotalQtrs() , o9_0);
+    assertEquals(klazz.getCurQtrs() , o9_1);
+    assertEquals(klazz.getAllowVend() , o9_2);
     assertEquals("empty", klazz.getStateFullName());
   }
 
   @Test
   public void testCase9() {
+    Object o2_0 = klazz.getCurQtrs() + 1;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o2_0);
     assertEquals("notAllowed", klazz.getStateFullName());
+    Object o5_0 = klazz.getCurQtrs() + 1;
+    Object o5_1 = true;
     klazz.addQtr();
+    assertEquals(klazz.getCurQtrs() , o5_0);
+    assertEquals(klazz.getAllowVend() , o5_1);
     assertEquals("allowed", klazz.getStateFullName());
     klazz.setCurQtrs(3);
+    Object o10_0 = klazz.getTotalQtrs() + 2;
+    Object o10_1 = 1;
+    Object o10_2 = false;
     klazz.vend();
+    assertEquals(klazz.getTotalQtrs() , o10_0);
+    assertEquals(klazz.getCurQtrs() , o10_1);
+    assertEquals(klazz.getAllowVend() , o10_2);
     assertEquals("notAllowed", klazz.getStateFullName());
   }
 
--- src/ca/mcgill/ecse429/conformancetest/generator/Generator.java
+++ src/ca/mcgill/ecse429/conformancetest/generator/Generator.java
@@ -1,8 +1,10 @@
 package ca.mcgill.ecse429.conformancetest.generator;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import ca.mcgill.ecse429.conformancetest.generator.tree.Node;
 import ca.mcgill.ecse429.conformancetest.generator.tree.RoundTripPathTree;
@@ -16,145 +18,243 @@ public class Generator {
 	private StateMachine sm;
 	private ArrayList<StringBuilder> testCases;
 	private String sourceFile;
-	
-	public Generator(String xmlFile, String sourceFile){
+	private int counter;
+
+	public Generator(String xmlFile, String sourceFile) {
 		PersistenceStateMachine.loadStateMachine(xmlFile);
+		this.counter = 0;
 		this.testSuite = new StringBuilder();
 		this.testCases = new ArrayList<StringBuilder>();
-		this.sourceFile= sourceFile;
+		this.sourceFile = sourceFile;
 	}
-	
-	public void generate(){
+
+	public void generate() {
 		this.sm = StateMachine.getInstance();
 		RoundTripPathTree tree = new RoundTripPathTree(this.sm);
 		buildTestCases(tree.getRoot());
 		exportToFile();
 	}
-	
-	private void exportToFile(){
-	    try {
-	    	File sourceFile = new File(this.sourceFile);
-	    	FileWriter writer = new FileWriter(sourceFile);
+
+	private void exportToFile() {
+		try {
+			File sourceFile = new File(this.sourceFile);
+			FileWriter writer = new FileWriter(sourceFile);
 			writer.write(testSuite.toString());
 			writer.close();
-	    } catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private void initializeTestCases(Node root, String className){		
+
+	private void initializeTestCases(Node root, String className) {
 		testSuite.append("package " + sm.getPackageName() + ";\n");
 		testSuite.append("import " + sm.getPackageName() + ".*;\n");
 		testSuite.append("import static org.junit.Assert.*;\n");
 		testSuite.append("import org.junit.Before;\n");
 		testSuite.append("import org.junit.Test;\n\n");
 		testSuite.append("public class " + className + "Test " + "{\n");
-		
-		indent(1); testSuite.append("private " + className + " klazz;\n\n");
-		indent(1); testSuite.append("@Before\n");
-		indent(1); testSuite.append("public void setUp() throws Exception {\n");
-		indent(2); testSuite.append("klazz = new " + className + "();\n");
-		
-		if(!root.moves.isEmpty()){
-			root.moves.get(0).trans.ifPresent(t -> verifyCurrentState(testSuite, t.getTo()));
+
+		indent(1);
+		testSuite.append("private " + className + " klazz;\n\n");
+		indent(1);
+		testSuite.append("@Before\n");
+		indent(1);
+		testSuite.append("public void setUp() throws Exception {\n");
+		indent(2);
+		testSuite.append("klazz = new " + className + "();\n");
+
+		if (!root.moves.isEmpty()) {
+			root.moves.get(0).trans.ifPresent(t -> verifyCurrentState(
+					testSuite, t.getTo()));
 		}
-		
-		indent(1); testSuite.append("}\n");
+
+		indent(1);
+		testSuite.append("}\n");
 	}
-	
-	private void finalizeTestCases(){
-		for (int i = 0; i < testCases.size(); i++){
-			indent(1); testSuite.append("@Test\n");
-			indent(1); testSuite.append("public void testCase" + i + "() {\n");
+
+	private void finalizeTestCases() {
+		for (int i = 0; i < testCases.size(); i++) {
+			indent(1);
+			testSuite.append("@Test\n");
+			indent(1);
+			testSuite.append("public void testCase" + i + "() {\n");
 			testSuite.append(testCases.get(i).toString());
-			indent(1); testSuite.append("}\n\n");
+			indent(1);
+			testSuite.append("}\n\n");
 		}
 		testSuite.append("}");
 	}
-	
-	private void buildTestCases(Node root){
-		String className = sm.getClassName().substring(0, sm.getClassName().indexOf('.'));
+
+	private void buildTestCases(Node root) {
+		String className = sm.getClassName().substring(0,
+				sm.getClassName().indexOf('.'));
 		initializeTestCases(root, className);
-		
+
 		StringBuilder sb = new StringBuilder();
 		testCases.add(sb);
-		if(!root.moves.isEmpty()){
+		if (!root.moves.isEmpty()) {
 			generateTestCases(sb, root.moves.get(0));
 		}
 		finalizeTestCases();
 	}
 	
-	private void generateTestCases(StringBuilder sb, Node n){
-		if(n.moves.isEmpty()){
+	private void handleTransition(StringBuilder s, Transition t){
+		setConditions(s, t);
+		int count = this.counter++;
+		getBeforeAction(s, t, count);
+		performNextTransition(s, t);
+		checkAction(s, t, count);
+		verifyCurrentState(s, t.getTo());
+	}
+
+	private void generateTestCases(StringBuilder sb, Node n) {
+		if (n.moves.isEmpty()) {
 			return;
 		}
-		
-		for(int i = 0; i < n.moves.size() - 1; i++){
+
+		for (int i = 0; i < n.moves.size() - 1; i++) {
 			StringBuilder s = new StringBuilder();
 			testCases.add(s);
 			s.append(sb.toString());
 			final int x = i;
 			n.moves.get(i).trans.ifPresent(t -> {
-				setConditions(s, t);
-				performNextTransition(s, t);
-				verifyCurrentState(s, t.getTo());
+				handleTransition(s, t);
 				generateTestCases(s, n.moves.get(x));
 			});
 		}
 		final int x = n.moves.size() - 1;
 		n.moves.get(x).trans.ifPresent(t -> {
-			setConditions(sb, t);
-			performNextTransition(sb, t);
-			verifyCurrentState(sb, t.getTo());
+			handleTransition(sb, t);
 			generateTestCases(sb, n.moves.get(x));
 		});
 	}
-	
-	private void verifyCurrentState(StringBuilder s, State t){
-		indent(s, 2); s.append("assertEquals(\"" + t.getName() + "\", klazz.getStateFullName());\n");
+
+	private void verifyCurrentState(StringBuilder s, State t) {
+		indent(s, 2);
+		s.append("assertEquals(\"" + t.getName()
+				+ "\", klazz.getStateFullName());\n");
 	}
-	
-	private void setConditions(StringBuilder s, Transition t){
-		if(t.getCondition().length() > 0){
-			String cond = t.getCondition();
-			if (cond.matches("^get.*\\(\\)$")){
-				indent(s, 2); s.append("klazz.set" + cond.substring(3, cond.length() - 1) + "true);\n");
-			} else if (cond.matches("^!get.*\\(\\)$")){
-				indent(s, 2); s.append("klazz.set" + cond.substring(4, cond.length() - 1) + "false);\n");
-			} else if (cond.matches(".+[<>=]=[ 0-9]+")){
-				String[] split = cond.split("[<>=]=");
-				String method = "klazz.set" + split[0].trim().substring(0, 1).toUpperCase() + split[0].trim().substring(1);
-				String value = split[1].trim();
-				indent(s, 2); s.append(method + "(" + value + ");\n");				
-			} else if (cond.matches(".+>[^=].+")){
-				String[] split = cond.split(">");
-				String method = "klazz.set" + split[0].trim().substring(0, 1).toUpperCase() + split[0].trim().substring(1);
-				String value = split[1].trim();
-				indent(s, 2); s.append(method + "(" + value + " + 1);\n");
-			} else if (cond.matches(".+<[^=].+")){
-				String[] split = cond.split(">");
-				String method = "klazz.set" + split[0].trim().substring(0, 1).toUpperCase() + split[0].trim().substring(1);
-				String value = split[1].trim();
-				indent(s, 2); s.append(method + "(" + value + " - 1);\n");
-			} else {
-				indent(s, 2); s.append("// COND: " + cond + "\n");
+
+	private void getBeforeAction(StringBuilder s, Transition t, int count){
+		String action = t.getAction();
+		int i = 0;
+		if (action.length() > 0) {
+			String[] actions = action.split(";");
+			for (String a : actions) {
+				if (a.length() > 0) {
+					String[] act = a.trim().split(" ?= ?");
+					if (act.length != 2) {
+						String x = "";
+						for (String y : act) {
+							x = x + ", " + y;
+						}
+						throw new RuntimeException("ERROR: " + x);
+					} else {
+						String method = act[0].substring(0, 1).toUpperCase()
+								+ act[0].substring(1);
+						String arg = act[1];
+						if (arg.trim()
+								.matches(act[0].trim() + " ?[+-] ?[0-9]+")) {
+							arg = arg.replace(act[0].trim(), "klazz.get"
+									+ method + "()");
+						}
+						
+						indent(s, 2); s.append("Object o" + count + "_" + i++ + " = " + arg + ";\n");
+//						s.append("// klazz.set" + method + "(" + arg + ");\n");
+					}
+				}
 			}
 		}
 	}
-		
-	private void performNextTransition(StringBuilder s, Transition t){
-		if(t.getEvent().length() > 0){
-			indent(s, 2); s.append("klazz." + t.getEvent() + "();\n");
+	
+	private void checkAction(StringBuilder s, Transition t, int count) {
+		String action = t.getAction();
+		int i = 0;
+		if (action.length() > 0) {
+			String[] actions = action.split(";");
+			for (String a : actions) {
+				if (a.length() > 0) {
+					String[] act = a.trim().split(" ?= ?");
+					if (act.length != 2) {
+						String x = "";
+						for (String y : act) {
+							x = x + ", " + y;
+						}
+						throw new RuntimeException("ERROR: " + x);
+					} else {
+						String method = act[0].substring(0, 1).toUpperCase()
+								+ act[0].substring(1);
+						String arg = act[1];
+						if (arg.trim()
+								.matches(act[0].trim() + " ?[+-] ?[0-9]+")) {
+							arg = arg.replace(act[0].trim(), "klazz.get"
+									+ method + "()");
+						}
+
+						indent(s, 2); s.append("assertEquals(klazz.get" + method+ "() , o" + count + "_" + i++ + ");\n");
+//						s.append("// klazz.set" + method + "(" + arg + ");\n");
+					}
+				}
+			}
 		}
 	}
-	
-	private void indent(StringBuilder test, int depth){
-		for (int i = 0; i < depth; i++){
+
+	private void setConditions(StringBuilder s, Transition t) {
+		if (t.getCondition().length() > 0) {
+			String cond = t.getCondition();
+			if (cond.matches("^get.*\\(\\)$")) {
+				indent(s, 2);
+				s.append("klazz.set" + cond.substring(3, cond.length() - 1)
+						+ "true);\n");
+			} else if (cond.matches("^!get.*\\(\\)$")) {
+				indent(s, 2);
+				s.append("klazz.set" + cond.substring(4, cond.length() - 1)
+						+ "false);\n");
+			} else if (cond.matches(".+[<>=]=[ 0-9]+")) {
+				String[] split = cond.split("[<>=]=");
+				String method = "klazz.set"
+						+ split[0].trim().substring(0, 1).toUpperCase()
+						+ split[0].trim().substring(1);
+				String value = split[1].trim();
+				indent(s, 2);
+				s.append(method + "(" + value + ");\n");
+			} else if (cond.matches(".+>[^=].+")) {
+				String[] split = cond.split(">");
+				String method = "klazz.set"
+						+ split[0].trim().substring(0, 1).toUpperCase()
+						+ split[0].trim().substring(1);
+				String value = split[1].trim();
+				indent(s, 2);
+				s.append(method + "(" + value + " + 1);\n");
+			} else if (cond.matches(".+<[^=].+")) {
+				String[] split = cond.split(">");
+				String method = "klazz.set"
+						+ split[0].trim().substring(0, 1).toUpperCase()
+						+ split[0].trim().substring(1);
+				String value = split[1].trim();
+				indent(s, 2);
+				s.append(method + "(" + value + " - 1);\n");
+			} else {
+				indent(s, 2);
+				s.append("// COND: " + cond + "\n");
+			}
+		}
+	}
+
+	private void performNextTransition(StringBuilder s, Transition t) {
+		if (t.getEvent().length() > 0) {
+			indent(s, 2);
+			s.append("klazz." + t.getEvent() + "();\n");
+		}
+	}
+
+	private void indent(StringBuilder test, int depth) {
+		for (int i = 0; i < depth; i++) {
 			test.append("  ");
 		}
 	}
-	
-	private void indent(int depth){
+
+	private void indent(int depth) {
 		indent(testSuite, depth);
 	}
 }
